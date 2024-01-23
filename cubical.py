f76e990a49e82c7a5167c328ec268b5d59d75fad
unknown <dejan.govc@gmail.com>
2024-01-23 03:35:21 +0100

    Added basic version of the code.

--- cubical.py
+++ cubical.py
@@ -1,244 +1,39 @@
 from functools import partial
 from multiprocessing import Pool
-import os
 import time
-import sqlite3 as s3
-import builtins
-
-import user_interface as ui
 import functions as fn
 
-# REDEFINE print FUNCTION TO FLUSH AUTOMATICALLY.
-def print(*args, **kwargs):
-    kwargs['flush'] = True
-    builtins.print(*args, **kwargs)
-
 def main():
-    ### SET PARAMETERS
-    n, chunksize, dbprefix, surf_type = ui.user_interface()
+    print("Started computation.")
+    n = 5 # DIMENSION OF CUBE, HAS TO BE SET MANUALLY.
     n2 = n*(n-1)*2**(n-3)
-    imin = 2 # default: 2
-    imax = n2 # default: n2
-    cplxs = [1]
-    lencplxs = 1
-    if dbprefix == "db":
-        extend_fun = partial(fn.disconnected_withbdry_extendonce,n=n)
-    elif dbprefix == "b":
-        extend_fun = partial(fn.withbdry_extendonce,n=n)
-    elif dbprefix == "dc":
-        extend_fun = partial(fn.disconnected_extendonce,n=n)
-    else:
-        extend_fun = partial(fn.extendonce,n=n)
-    label_fun = partial(fn.cubicalcanlabel,n=n)
-    bool_fun = partial(fn.testedges,n=n)
-
-    db_name = dbprefix+f'cplxs{n}.db'
-    if os.path.exists(db_name):
-        response = input(f"WARNING!!! Database file {db_name} already exists.\nOverwrite [O], continue existing computation [C] or quit [Q]? ").lower()
-        if response == 'o':
-            print("Overwriting database file.")
-            os.remove(db_name)
-        elif response == "c":
-            print("Resuming computation ...",end="\r")
-        else:
-            print("Cannot proceed without permission. Exiting computation.")
-            exit()
-
-    ### CREATE AND INITIALIZE DATABASE (OR ATTEMPT TO CONTINUE PREVIOUS COMPUTATION)
-    t0 = time.time()
-
-    conn = s3.connect(db_name)
-    cursor = conn.cursor()
-
-    cursor.execute("PRAGMA integrity_check")
-    if cursor.fetchone()[0] != "ok":
-        print("ERROR: Integrity of the database has been compromised. Exiting computation.")
-        exit()
-
-    #cursor.execute("PRAGMA journal_mode = WAL")
-    #cursor.execute("PRAGMA synchronous = NORMAL")
-    #cursor.execute("PRAGMA journal_size_limit = 6144000")
-    #cursor.execute("PRAGMA cache_size = -32000")
-    #cursor.execute("PRAGMA temp_store = MEMORY")    
-    #cursor.execute("PRAGMA mmap_size = 30000000000")
-
-    cursor.execute("SELECT name FROM sqlite_master WHERE type='table'")
-    tab = [s for t in cursor.fetchall() for s in t]
-    if tab != []:
-        tab1 = [eval(s[5:]) for s in tab if s[:5] == "cplxs"]
-        if tab1 == []:
-            print("The computation is already complete. Exiting computation.")
-            exit()
-        imin = max(tab1)+1
-        table_name = "cplxs"+str(imin-1)
-        cursor.execute(f"SELECT COUNT(*) FROM {table_name}")
-        lencplxs = cursor.fetchone()[0]
-        for t in tab:
-            if t != table_name and t != "goodcplxs":
-                cursor.execute(f"DROP TABLE IF EXISTS {t}")
-    
-    if imin == 2:
-        cursor.execute("BEGIN TRANSACTION")
-        cursor.execute("DROP TABLE IF EXISTS cplxs1")
-        cursor.execute("DROP TABLE IF EXISTS goodcplxs")
-        cursor.execute("""
-            CREATE TABLE IF NOT EXISTS goodcplxs (
-                id INTEGER PRIMARY KEY,
-                integer1 BLOB
-            )
-        """)
-        if dbprefix == "b" or dbprefix == "db":
-            cursor.execute("INSERT INTO goodcplxs (integer1) VALUES (?)", 
-                        (fn.int_to_blob(cplxs[0],n2),))
-        cursor.execute("""
-            CREATE TABLE IF NOT EXISTS cplxs1 (
-                id INTEGER PRIMARY KEY,
-                integer1 BLOB
-            )
-        """)
-        for val1 in cplxs:
-            cursor.execute("INSERT INTO cplxs1 (integer1) VALUES (?)", 
-                        (fn.int_to_blob(val1,n2),))
-        conn.commit()
-
-    print(f"Enumerating {surf_type}; n = {n}, chunk size = {chunksize}.\nStoring data to database {db_name}.")
-
-    write_to_text = True ### CHANGE THIS TO False TO DISABLE .txt OUTPUT
-    print("WARNING!!! Also storing final result as a .txt file.")
-
-    ### MAIN LOOP OF THE COMPUTATION (STARTING FROM POINT OF INTERRUPTION IF NEEDED)
-    for i in range(imin,imax+1):
-
-        table_name = f"cplxs{i-1}"
-        cursor.execute(f"SELECT * FROM {table_name} ORDER BY id")
-        labeling_cursor = conn.cursor()
-        cc = 0
-        ctot = lencplxs//chunksize+1
-        lencplxs = 0
-        while True:
-            cc += 1
-            rows = cursor.fetchmany(chunksize)
-            if not rows:
-                break
-            cplxs = [fn.blob_to_int(r[1]) for r in rows]
-            labeling_cursor.execute("""
-                CREATE TABLE IF NOT EXISTS labeledcplxs (
-                    id INTEGER PRIMARY KEY,
-                    integer1 BLOB,
-                    integer2 BLOB,
-                    integer3 INTEGER NOT NULL CHECK (integer3 IN (0, 1)),
-                    integer4 INTEGER NOT NULL CHECK (integer3 IN (0, 1)))
-            """)
-            with Pool() as p:
-                cplxs = p.map(extend_fun,cplxs)
-            cplxs = [c for cxs in cplxs for c in cxs]
-            lencplxs += len(cplxs)
-            print(" "*120,end="\r")
-            print(time.ctime()+f": Computing chunk {cc}/{ctot}. Done extending ({len(cplxs)}). ",end="")
-            with Pool() as p:
-                labels = p.map(label_fun, cplxs)
-            print("Done labeling. ",end="")
-            with Pool() as p:
-                bool = p.map(bool_fun, cplxs)
-            bool1 = [b[0] and b[1] for b in bool]
-            bool2 = [b[0] and not b[1] for b in bool]
-            if dbprefix == "b" or dbprefix == "db":
-                bool1 = [b[0] for b in bool]
-                bool2 = bool1
-            if dbprefix == "dc":
-                bool1 = [b[0] and b[1] for b in bool]
-                bool2 = [b[0] for b in bool]
-            print("Done testing.",end="\r")
-            for val1, val2, val3, val4 in zip(cplxs, labels, bool1, bool2):
-                labeling_cursor.execute("INSERT INTO labeledcplxs (integer1, integer2, integer3, integer4) VALUES (?, ?, ?, ?)", 
-                            (fn.int_to_blob(val1,n2), fn.int_to_blob(val2,n2), val3, val4))
-        labeling_cursor.close()
-        cursor.execute(f"""
-            DROP TABLE IF EXISTS cplxs{i-1}
-        """)
-        conn.commit()
-        cursor.execute("VACUUM")
-                
-        print(" "*120,end="\r")
-        print(time.ctime()+f": All chunks extended, labeled & tested ({lencplxs}). Data stored.",end="\r")
-
-        cursor.execute("CREATE INDEX idx_integer2_id ON labeledcplxs(integer2, id)")
-
-        cursor.execute("""
-            CREATE TABLE uniquecplxs AS
-            SELECT l.id, l.integer1, l.integer2, l.integer3, l.integer4
-            FROM labeledcplxs l
-            INNER JOIN (
-                SELECT integer2, MIN(id) as min_id
-                FROM labeledcplxs
-                GROUP BY integer2
-            ) as subquery
-            ON l.integer2 = subquery.integer2 AND l.id = subquery.min_id
-        """)
-        cursor.execute("""
-            DROP TABLE IF EXISTS labeledcplxs
-        """)
-        conn.commit()
-        cursor.execute("SELECT COUNT(*) FROM uniquecplxs")
-        lenunique = cursor.fetchone()[0]
-        print(" "*120,end="\r")
-        print(time.ctime()+f": Done reducing ({lenunique}). ",end="")
-
-        table_name = f"cplxs{i}"
-        cursor.execute("BEGIN TRANSACTION")
-        cursor.execute(f"""
-            CREATE TABLE IF NOT EXISTS {table_name} (
-                id INTEGER PRIMARY KEY,
-                integer1 BLOB
-            )
-        """)
-        cursor.execute(f"""
-            INSERT INTO {table_name} (integer1)
-            SELECT integer1
-            FROM uniquecplxs
-            WHERE integer4
-            ORDER BY id;
-        """)
-        cursor.execute(f"""
-            INSERT INTO goodcplxs (integer1)
-            SELECT integer1
-            FROM uniquecplxs
-            WHERE integer3
-            ORDER BY id;
-        """)
-        cursor.execute("""
-            DROP TABLE IF EXISTS uniquecplxs
-        """)
-        print("Data stored.",end="\r")
-        conn.commit()
-        cursor.execute("VACUUM")
-
-        cursor.execute(f"SELECT COUNT(*) FROM {table_name}")
-        lencplxs = cursor.fetchone()[0]
-        cursor.execute(f"SELECT COUNT(*) FROM goodcplxs")
-        lengoodcplxs = cursor.fetchone()[0]
-
-        print(time.ctime()+": # squares: "+str(i)+"; # cplxs: "+str(lencplxs)+"; # goodcplxs: "+str(lengoodcplxs))
-        if lencplxs == 0:
-            cursor.execute(f"""
-                DROP TABLE IF EXISTS cplxs{i}
-            """)
-            print("Vacuuming ...",end="\r")
-            cursor.execute("VACUUM")
+    tab = fn.cubes(n,2)
+    cplxs = [fn.zeroone([tuple(tab[0])],tab)]
+    goodcplxs = []
+    bool1_fun = partial(fn.zeroonevalidq,tab=tab)
+    bool2_fun = partial(fn.zeroonesaturatedq,tab=tab)
+    print("Variables loaded.")
+    for i in range(-1+1,n2):
+        cplxs = fn.cubicalextend(cplxs,tab,n)
+        #print(time.ctime()+": Done extending.")
+        cplxs = fn.cubicalisoreduce(cplxs,tab,n)
+        with Pool() as p:
+            bool1 = p.map(bool1_fun, cplxs)
+            bool1 = [b for b in bool1]
+            bool2 = p.map(bool2_fun, cplxs)
+            bool2 = [b for b in bool2]    
+        bool3 = [val1 and val2 for val1, val2 in zip(bool1,bool2)]
+        bool4 = [val1 and not val2 for val1, val2 in zip(bool1,bool2)]
+        goodcplxs += [c for c, val in zip(cplxs, bool3) if val]
+        cplxs = [c for c, val in zip(cplxs, bool4) if val]
+        print(time.ctime()+": # squares: "+str(i+2)+"; # cplxs: "+str(len(cplxs))+"; # goodcplxs: "+str(len(goodcplxs)))
+        if cplxs == []:
             break
-    print("Total number of goodcplxs: "+str(lengoodcplxs))
-
-    if write_to_text:
-        cursor.execute("SELECT * FROM goodcplxs ORDER BY id")
-        writecplxs = cursor.fetchall()
-        writestring = "".join([str(fn.fromzeroone(fn.blob_to_int(c[1]),fn.cubes(n,2)))+"\n" for c in writecplxs])
-        g = open(db_name[:-3]+"_result.txt","w")
-        g.write(writestring)
-        g.close()
-    cursor.close()
-    conn.close()
-
-    print("Total time of computation: "+str(time.time()-t0))
+    print("Total number of goodcplxs: "+str(len(goodcplxs)))
+    writestring = "".join([str(c)+"\n" for c in goodcplxs])
+    g = open("cplxs"+str(n)+".txt","w")
+    g.write(writestring)
+    g.close()
 
 if __name__ == "__main__":
     main()
--- functions.py
+++ functions.py
@@ -1,58 +1,11 @@
+#import time
+from multiprocessing import Pool
+from collections import Counter
 from functools import lru_cache
+from functools import partial
 from itertools import product
 from itertools import groupby
 from itertools import permutations
-
-### FORMATS FOR SQLITE3:
-
-def int_to_blob(val,n2):
-    num_bytes = n2//8  #8 bits per byte
-    return val.to_bytes(num_bytes, byteorder='big', signed=False)
-
-def blob_to_int(val):
-    return int.from_bytes(val, byteorder='big', signed=False)
-
-### PERMUTATIONS:
-
-def sorting_perm(lst):
-    sorted_pairs = sorted(enumerate(lst), key=lambda x: x[1])
-    permutation = tuple(index for index, _ in sorted_pairs)
-    return permutation
-
-def repeats(lst):
-    tab = sorted(lst)
-    counts = tuple(len(list(group)) for _, group in groupby(tab))
-    return counts
-    
-@lru_cache(maxsize=None)
-def perms(rptlist):
-    permlist = [list(permutations(range(p))) for p in rptlist]
-    for i in range(len(rptlist)-1):
-        m = max(permlist[-2][0])
-        permlist[-1] = [[j+m+1 for j in s] for s in permlist[-1]]
-        permlist[-2] = [list(p)+list(s) for p in permlist[-2] for s in permlist[-1]]
-        permlist = permlist[:-1]
-    return tuple(tuple(p) for p in permlist[0])
-
-### DIFFERENT ENCODINGS OF COMPLEXES:
-
-def zeroone(lst,tab):
-    zo = 0
-    for e in lst:
-        zo |= (1 << tab.index(e))
-    return zo
-
-def fromzeroone(zo,tab):
-    fromzo = []
-    lst = [int(bit) for bit in bin(zo)[2:]]
-    lst.reverse()
-    lst += [0 for _ in range(len(tab)-len(lst))]
-    for i in range(len(lst)):
-        if lst[i] == 1:
-            fromzo.append(tab[i])
-    return(sorted(fromzo))
-
-### FACES, BOUNDARIES, DEGREES:
 
 @lru_cache(maxsize=None)
 def cubes(n,k=None):
@@ -75,102 +28,99 @@ def bdry(c):
 def bbdry(c):
     return tuple(sorted(list(set(v for s in bdry(c) for v in bdry(s)))))
 
-@lru_cache(maxsize=None)
-def boundaries(n):
-    return tuple(zeroone(bdry(c),cubes(n,1)) for c in cubes(n,2))
+def edgedegs(cplx):
+    return list(Counter([e for c in cplx for e in bdry(c)]).values())
+
+def badedges(cplx):
+    return sorted([c for c,val in Counter([e for c in cplx for e in bdry(c)]).items() if val < 2])
+
+### PERMUTATIONS:
+
+"""def padding(t,l):
+    return tuple(-1 for i in range(l-len(t)))+t
+
+def sorting_perm(lst):
+    l = max([len(t) for t in lst])
+    extlist = [padding(t,l) for t in lst]
+    extlist = [extlist[i]+(i,) for i in range(len(lst))]
+    perm = [extlist.index(v) for v in sorted(extlist)]
+    return perm
+
+def repeats(lst):
+    perm = sorting_perm(lst)
+    lst = [lst[p] for p in perm]
+    ordlist = sorted(set([lst.index(v) for v in lst]))
+    ordlist = ordlist+[len(lst)]
+    return [ordlist[i+1]-ordlist[i] for i in range(len(ordlist)-1)]
 
 @lru_cache(maxsize=None)
-def bboundaries(n):
-    return tuple(zeroone(bbdry(c),cubes(n,0)) for c in cubes(n,2))
+def perms(rptlist):
+    permlist = [list(permutations(range(p))) for p in rptlist]
+    for i in range(len(rptlist)-1):
+        m = max(permlist[-2][0])
+        permlist[-1] = [[j+m+1 for j in s] for s in permlist[-1]]
+        permlist[-2] = [list(p)+list(s) for p in permlist[-2] for s in permlist[-1]]
+        permlist = permlist[:-1]
+    return permlist[0]"""
 
-def testedges(ncplx,n):
-    n2 = n*(n-1)*2**(n-3)
-    once = 0
-    twice = 0
-    for i in range(n2):
-        if ncplx & (1 << i):
-            twice |= once & boundaries(n)[i]
-            once ^= boundaries(n)[i]
-            if once & twice:
-                return (False,False)
-    return (True,once==0,once)
+def sorting_perm(lst):
+    sorted_pairs = sorted(enumerate(lst), key=lambda x: x[1])
+    permutation = tuple(index for index, _ in sorted_pairs)
+    return permutation
 
-def alledges(ncplx,n):
-    n2 = n*(n-1)*2**(n-3)
-    edges = 0
-    for i in range(n2):
-        if ncplx & (1 << i):
-            edges |= boundaries(n)[i]
-    return edges
-
-def firstone(n):
-    if n == 0:
-        raise ValueError("Zero does not have a first nonzero bit.")
-    index = 0
-    while True:
-        if (n >> index) & 1:
-            return index
-        else:
-            index += 1
-
+def repeats(lst):
+    tab = sorted(lst)
+    counts = tuple(len(list(group)) for _, group in groupby(tab))
+    return counts
+    
 @lru_cache(maxsize=None)
-def edgesquares(k,n):
-    return zeroone([c for c in cubes(n,2) if cubes(n,1)[k] in bdry(c)],cubes(n,2))
+def perms(rptlist):
+    permlist = [list(permutations(range(p))) for p in rptlist]
+    for i in range(len(rptlist)-1):
+        m = max(permlist[-2][0])
+        permlist[-1] = [[j+m+1 for j in s] for s in permlist[-1]]
+        permlist[-2] = [list(p)+list(s) for p in permlist[-2] for s in permlist[-1]]
+        permlist = permlist[:-1]
+    return tuple(tuple(p) for p in permlist[0])
 
-### AUTOMORPHISMS AND CANONICAL LABELING (assuming testedges(ncplx,n)[0]==True and n <= 7):
+### AUTOMORPHISMS:
 
-@lru_cache(maxsize=None)
-def edgeboundaries(n):
-    return tuple(zeroone(bdry(c),cubes(n,0)) for c in cubes(n,1))
-
-def fake_octal(x):
-    return int(bin(x)[2:],8)
-
-def testvertices(ncplx,n):
-    n2 = n*(n-1)*2**(n-3)
-    n1 = n*2**(n-1)
-    n0 = 2**n
-    edges = alledges(ncplx,n)
-    vine = 0
-    vins = 0
-    for i in range(n1):
-        if edges & (1 << i):
-            vine += fake_octal(edgeboundaries(n)[i])
-    for i in range(n2):
-        if ncplx & (1 << i):
-            vins += fake_octal(bboundaries(n)[i])
-    tupvine = tuple(int(i) for i in oct(vine)[2:])
-    tupvine = tuple(0 for _ in range(n0-len(tupvine))) + tupvine
-    tupvins = tuple(int(i) for i in oct(vins)[2:])
-    tupvins = tuple(0 for _ in range(n0-len(tupvins))) + tupvins
-    return tuple(reversed(tuple(zip(tupvine,tupvins))))
+def ddegs(cplx):
+    vertices = cubes(len(cplx[0]),0)
+    squares = sorted(cplx)
+    edges = sorted(list(set([t for c in cplx for t in bdry(c)])))
+    vine = Counter([t for c in edges for t in bdry(c)])
+    vins = Counter([t for c in squares for t in bbdry(c)])
+    combined = [(vine.get(v,0),vins.get(v,0)) for v in vertices]
+    return combined
 
 @lru_cache(maxsize=None)
 def nbhd(v):
     return tuple(v[:i]+(1-v[i],)+v[i+1:] for i in range(len(v)))
 
-@lru_cache(maxsize=None)
-def nnbhd(i,n):
-    v = cubes(n,0)[i]
-    return tuple([cubes(n,0).index(u) for u in nbhd(v)])
+def bigdegs(cplx):
+    vertices = cubes(len(cplx[0]),0)
+    dd = ddegs(cplx)
+    ddict = dict(zip(vertices,dd))
+    return [ddict[v]+tuple(i for t in sorted([ddict[u] for u in nbhd(v)]) for i in t) for v in vertices]
 
-def bigdegs(ncplx,n):
-    dd = testvertices(ncplx,n)
-    return tuple([dd[v]+tuple(i for t in sorted([dd[u] for u in nnbhd(v,n)]) for i in t) for v in range(1<<n)])
-
-def bigbigdegs(ncplx,n):
-    dd = bigdegs(ncplx,n)
-    return tuple([dd[v]+tuple(i for t in sorted([dd[u] for u in nnbhd(v,n)]) for i in t) for v in range(1<<n)])
+def bigbigdegs(cplx):
+    vertices = cubes(len(cplx[0]),0)
+    dd = bigdegs(cplx)
+    ddict = dict(zip(vertices,dd))
+    return [ddict[v]+tuple(i for t in sorted([ddict[u] for u in nbhd(v)]) for i in t) for v in vertices]
 
 def pairs(dd,n):
-    mindeg = min(dd)
-    minverts = [v for v in range(1<<n) if dd[v] == mindeg]
+    vertices = cubes(n,0)
+    mindeg = sorted(dd)[0]
+    ddict = dict(zip(vertices,dd))
+    minverts = [v for v,val in ddict.items() if val == mindeg]
     pp = []
     for v in minverts:
-        nbdegs = [dd[u] for u in nnbhd(v,n)]
+        nbdegs = [ddict[u] for u in nbhd(v)]
         pi = sorting_perm(nbdegs)
         permlist = [tuple(pi[i] for i in tau) for tau in perms(repeats(nbdegs))]
-        pp += [(cubes(n,0)[v],tau) for tau in permlist]
+        pp += [(v,tau) for tau in permlist]
     return tuple(pp)
 
 @lru_cache(maxsize=None)
@@ -181,68 +131,71 @@ def renamecube(c,p):
 def rename(cplx,p):
     return [renamecube(c,p) for c in cplx]
 
-@lru_cache(maxsize=None)
-def faceperm(p,n):
-    squareindices = {c:index for index,c in enumerate(cubes(n,2))}
-    return tuple(squareindices[renamecube(c,p)] for c in cubes(n,2))
+### DIFFERENT ENCODINGS OF COMPLEXES:
 
-def apply(perm,ncplx,n):
-    permcplx = 0
-    n2 = n*(n-1)*2**(n-3)
-    for i in range(n2):
-        if ncplx & (1 << i):
-            permcplx |= (1 << perm[i])
-    return permcplx
+def zeroone(lst,tab):
+    zo = 0
+    for e in lst:
+        zo += 2**(tab.index(e))
+    return zo
 
-def cubicalcanlabel(ncplx,n):
-    dd = bigbigdegs(ncplx,n)
+def fromzeroone(zo,tab):
+    fromzo = []
+    lst = [int(bit) for bit in bin(zo)[2:]]
+    lst.reverse()
+    lst += [0 for i in range(len(tab)-len(lst))]
+    for i in range(len(lst)):
+        if lst[i] == 1:
+            fromzo.append(tab[i])
+    return(sorted(fromzo))
+
+### EXTENSION AND REDUCTION:
+
+def extendonce(ncplx,tab,n):
+    ncplxsnew = []
+    cplx = fromzeroone(ncplx,tab)
+    e = badedges(cplx)[0]
+    for i in reversed(range(n)):
+        f = e[:i]+(2,)+e[i+1:]
+        if f == e:
+            continue
+        if f in cplx:
+            continue
+        ncplxsnew += [zeroone(cplx + [f],tab)]
+    return ncplxsnew
+
+def cubicalextend(ncplxs,tab,n):
+    extendfun = partial(extendonce,tab=tab,n=n)
+    with Pool() as p:
+        ncplxsnew = p.map(extendfun,ncplxs)
+    ncplxsnew = [c for ncplxs in ncplxsnew for c in ncplxs]
+    return ncplxsnew
+
+def cubicalcanlabel(ncplx,tab,n):
+    cplx = fromzeroone(ncplx,tab)
+    dd = bigbigdegs(cplx)
     prs = pairs(dd,n)
-    return sorted([apply(faceperm(p,n),ncplx,n) for p in prs])[0]
+    #print(str(len(prs))+"   ",end="\r")
+    return zeroone(sorted([sorted(rename(cplx,p)) for p in prs])[0],tab)
 
-### EXTENSION (FOR VARIOUS TYPES OF SURFACES):
+def cubicalisoreduce(ncplxs,tab,n):
+    label_fun = partial(cubicalcanlabel,tab=tab,n=n)
+    with Pool() as p:
+        labels = p.map(label_fun, ncplxs)
+        labels = [l for l in labels]
+    #print(time.ctime()+": Done labeling.")
+    indices = {}
+    for index, number in enumerate(labels):
+        if number not in indices:
+            indices[number] = index
+    indices = list(indices.values())
+    #print(time.ctime()+": Done comparing.")
+    return [ncplxs[i] for i in indices]
 
-def extendonce(ncplx,n):
-    ncplxsnew = []
-    n2 = n*(n-1)*2**(n-3)
-    es = edgesquares(firstone(testedges(ncplx,n)[2]),n)
-    for i in range(n2):
-        if es & (1 << i) and not ncplx & (1 << i):
-            ncplxsnew.append(ncplx|(1<<i))
-    return ncplxsnew
+def zeroonesaturatedq(ncplx,tab):
+    cplx = fromzeroone(ncplx,tab)
+    return min(edgedegs(cplx))>=2
 
-def disconnected_withbdry_extendonce(ncplx,n):
-    ncplxsnew = []
-    n2 = n*(n-1)*2**(n-3)
-    for i in range(n2):
-        if not ncplx & (1 << i):
-            ncplxsnew.append(ncplx|(1<<i))
-    return ncplxsnew
-
-def withbdry_extendonce(ncplx,n):
-    ncplxsnew = []
-    n1 = n*2**(n-1)
-    n2 = n*(n-1)*2**(n-3)
-    es = 0
-    t = testedges(ncplx,n)[2]
-    for i in range(n1):
-        if (t >> i) & 1:
-            es |= edgesquares(i,n)
-    for i in range(n2):
-        if es & (1 << i) and not ncplx & (1 << i):
-            ncplxsnew.append(ncplx|(1<<i))
-    return ncplxsnew
-
-def disconnected_extendonce(ncplx,n):
-    ncplxsnew = []
-    n2 = n*(n-1)*2**(n-3)
-    t = testedges(ncplx,n)[2]
-    if t == 0:
-        for i in range(n2):
-            if not ncplx & (1 << i):
-                ncplxsnew.append(ncplx|(1<<i))
-        return ncplxsnew
-    es = edgesquares(firstone(t),n)
-    for i in range(n2):
-        if es & (1 << i) and not ncplx & (1 << i):
-            ncplxsnew.append(ncplx|(1<<i))
-    return ncplxsnew
+def zeroonevalidq(ncplx,tab):
+    cplx = fromzeroone(ncplx,tab)
+    return max(edgedegs(cplx))<=2
